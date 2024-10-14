6cd292ce175c8c7660319e0de58721e419e50fc7
Jasper Kooij <jasperkooij@gmail.com>
2024-10-14 14:36:31 -0500

    remove non-working image

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,12 +1,11 @@
 import type { Pokemon } from '@/data';
-
 export default function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
   return (
     <a href={`/pokemon/${pokemon.slug}`} class="block">
       <article class="group bg-flex flex-col sm:w-64 w-1/4 bg-white shadow-sm rounded-lg overflow-hidden hover:shadow-xl hover:shadow-gray-100 transition-all">
         <div class="sm:w-64 w-1/4 overflow-hidden">
           <img
-              src={`/_next/image?url=${pokemon.cover}&w=600&q=75`}
+              src={pokemon.cover}
               alt={pokemon.name}
             class="object-cover object-center w-full grayscale-[0.1] group-hover:grayscale-0 h-full rounded-md group-hover:scale-105 transition-all"
           />
