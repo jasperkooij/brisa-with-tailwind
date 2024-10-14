abee80d4fe36b6d6945cffbd8615784640f0b233
Jasper Kooij <jasperkooij@gmail.com>
2024-10-14 08:23:12 -0500

    first commit

---
+++ brisa.config.ts
@@ -0,0 +1,13 @@
+import type { Configuration } from 'brisa';
+import tailwindcss from 'brisa-tailwindcss';
+
+/// file: brisa.config.ts
+import vercel from 'brisa-adapter-vercel';
+
+export default {
+  output: 'node', // or 'static'
+  integrations: [tailwindcss()],
+  outputAdapter: vercel({
+    // see below for options that can be set here
+  })
+} satisfies Configuration;
---
+++ build/css-files.js
@@ -0,0 +1,1 @@
+export default ["layout-rn9ch6df.css"]
